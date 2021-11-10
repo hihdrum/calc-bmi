e1dc2e77648d4b0e64c0e35fe41f0f4aabb2491d
makoto <88492711+hihdrum@users.noreply.github.com>
2021-11-11 01:14:18 +0900

    講座4 型チェックの内容で更新した。

--- calc-bmi.lisp
+++ calc-bmi.lisp
@@ -4,6 +4,9 @@
 
 (in-package #:calc-bmi)
 
+(deftype positive-integer ()
+  '(integer 0 *))
+
 (defun to-string-result (value)
   (format nil "~,2f" value))
 
@@ -11,6 +14,8 @@
   (/ value 100))
 
 (defun calc-bmi (height weight)
+  (check-type height positive-integer)
+  (check-type weight positive-integer)
   (to-string-result
    (/ (float weight)
       (expt (float (to-meter height)) 2))))
--- test-calc-bmi.lisp
+++ test-calc-bmi.lisp
@@ -12,3 +12,30 @@
            (ok (string= (calc-bmi 165 58) "21.30")))
   (testing "Height: 170, Weight 60, Result: 20.76"
            (ok (string= (calc-bmi 170 60) "20.76"))))
+
+(deftest test-arguments-type-check ()
+  (flet ((create-expected-value (target value type)
+           (format nil
+                   "The value of CALC-BMI::~A is ~A, which is not of type CALC-BMI::~A."
+                   target value type))
+         (wrong-arguments-test (height weight expected-value)
+           (handler-case (calc-bmi height weight)
+             (error (condition)
+               (ok (string= (format nil "~A" condition) expected-value))))))
+    (testing "Should be an error expect for integer type"
+      (testing "When string is specified in the arg height"
+        (wrong-arguments-test
+         "160" 50
+         (create-expected-value "HEIGHT" "\"160\"" "POSITIVE-INTEGER")))
+      (testing "When string is specified in the arg weight"
+        (wrong-arguments-test
+         160 "50"
+         (create-expected-value "WEIGHT" "\"50\"" "POSITIVE-INTEGER")))
+      (testing "When negative integer is specified in the arg height"
+        (wrong-arguments-test
+         -1 50
+         (create-expected-value "HEIGHT" "-1" "POSITIVE-INTEGER")))
+      (testing "When negative integer is specified in the arg weight"
+        (wrong-arguments-test
+         160 -1
+         (create-expected-value "WEIGHT" "-1" "POSITIVE-INTEGER"))))))
